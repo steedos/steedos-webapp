d7ca34aea6046dc648e20f681d441bc9b2e1edb1
yinlainghui <yinlianghui@hotoa.com>
2019-09-26 10:24:14 +0800

    写死面板各个模块位置

--- src/components/dashboard/slds_dashboard.tsx
+++ src/components/dashboard/slds_dashboard.tsx
@@ -19,9 +19,21 @@ let Container = styled.div`
     flex-wrap: wrap;
 `;
 
+let Column = styled.div`
+    display: flex;
+    flex: 1;
+    flex-wrap: wrap;
+    &:nth-child(2){
+        flex: 0 0 33%;
+    }
+`;
+
 let Cell = styled.div`
-    flex: 0 0 50%;
+    flex: 0 0 100%;
     padding: 12px;
+    &.flex-split{
+        flex: 0 0 50%;
+    }
 `;
 
 class Dashboard extends React.Component {
@@ -72,31 +84,110 @@ class Dashboard extends React.Component {
         
         return (
             <Container className="slds-dashboard">
-                <Cell className="slds-dashboard-cell">
+                <Column className="slds-dashboard-column">
+                    <Cell className="slds-dashboard-cell">
+                        <IconSettings iconPath="/icons">
+                            <div className="slds-grid slds-grid_vertical">
+                                <Card
+                                    id="ExampleCard"
+                                    heading="今天的任务"
+                                    icon={<Icon category="standard" name="document" size="small" />}
+                                >
+                                    <Grid searchMode="omitFilters"
+                                        pageSize={200} 
+                                        objectName='organizations' 
+                                        columns={cellListColumns} 
+                                        selectionLabel={selectionLabel} 
+                                        $select={$select} 
+                                    />
+                                </Card>
+                            </div>
+                        </IconSettings>
+                    </Cell>
+                    <Cell className="slds-dashboard-cell">
+                        <IconSettings iconPath="/icons">
+                            <div className="slds-grid slds-grid_vertical">
+                                <Card
+                                    id="ExampleCard"
+                                    heading="今日事件"
+                                    icon={<Icon category="standard" name="document" size="small" />}
+                                >
+                                    <DataTable items={this.state.items} id="DataTableExample-1">
+                                        <DataTableColumn
+                                            label="Opportunity Name"
+                                            property="name"
+                                            truncate
+                                        />
+                                    </DataTable>
+                                </Card>
+                            </div>
+                        </IconSettings>
+                    </Cell>
+                    <Cell className="slds-dashboard-cell">
+                        <IconSettings iconPath="/icons">
+                            <div className="slds-grid slds-grid_vertical">
+                                <Card
+                                    id="ExampleCard"
+                                    heading="应用程序启动器"
+                                    icon={<Icon category="standard" name="document" size="small" />}
+                                >
+                                    <DataTable items={this.state.items} id="DataTableExample-1">
+                                        <DataTableColumn
+                                            label="Opportunity Name"
+                                            property="name"
+                                            truncate
+                                        />
+                                    </DataTable>
+                                </Card>
+                            </div>
+                        </IconSettings>
+                    </Cell>
+                    <Cell className="slds-dashboard-cell flex-split">
+                        <IconSettings iconPath="/icons">
+                            <div className="slds-grid slds-grid_vertical">
+                                <Card
+                                    id="ExampleCard"
+                                    heading="左下1"
+                                    icon={<Icon category="standard" name="document" size="small" />}
+                                >
+                                    <DataTable items={this.state.items} id="DataTableExample-1">
+                                        <DataTableColumn
+                                            label="Opportunity Name"
+                                            property="name"
+                                            truncate
+                                        />
+                                    </DataTable>
+                                </Card>
+                            </div>
+                        </IconSettings>
+                    </Cell>
+                    <Cell className="slds-dashboard-cell flex-split">
+                        <IconSettings iconPath="/icons">
+                            <div className="slds-grid slds-grid_vertical">
+                                <Card
+                                    id="ExampleCard"
+                                    heading="左下2"
+                                    icon={<Icon category="standard" name="document" size="small" />}
+                                >
+                                    <DataTable items={this.state.items} id="DataTableExample-1">
+                                        <DataTableColumn
+                                            label="Opportunity Name"
+                                            property="name"
+                                            truncate
+                                        />
+                                    </DataTable>
+                                </Card>
+                            </div>
+                        </IconSettings>
+                    </Cell>
+                </Column>
+                <Column className="slds-dashboard-column">
+                    <Cell className="slds-dashboard-cell">
                     <IconSettings iconPath="/icons">
                         <div className="slds-grid slds-grid_vertical">
                             <Card
                                 id="ExampleCard"
-                                heading="今天的任务"
-                                icon={<Icon category="standard" name="document" size="small" />}
-                            >
-                                <Grid searchMode="omitFilters"
-                                    pageSize={200} 
-                                    objectName='organizations' 
-                                    columns={cellListColumns} 
-                                    selectionLabel={selectionLabel} 
-                                    $select={$select} 
-                                />
-                            </Card>
-                        </div>
-                    </IconSettings>
-                </Cell>
-                <Cell className="slds-dashboard-cell">
-                    <IconSettings iconPath="/icons">
-                        <div className="slds-grid slds-grid_vertical">
-                            <Card
-                                id="ExampleCard"
-                                heading="今日事件"
+                                heading="右侧Card"
                                 icon={<Icon category="standard" name="document" size="small" />}
                             >
                                 <DataTable items={this.state.items} id="DataTableExample-1">
@@ -110,6 +201,7 @@ class Dashboard extends React.Component {
                         </div>
                     </IconSettings>
                 </Cell>
+                </Column>
             </Container>
         );
     }
