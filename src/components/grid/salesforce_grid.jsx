f3f04e9fc43500c8ce9d24bda3103f30071e69f6
baozhoutao <baozhoutao@hotoa.com>
2019-09-27 16:34:55 +0800

    清理logs

--- src/components/grid/salesforce_grid.jsx
+++ src/components/grid/salesforce_grid.jsx
@@ -11,7 +11,6 @@ let Counter = styled.div`
 `
 
 const CustomDataTableCell = ({ children, ...props }) => {
-	console.log('CustomDataTableCell', children);
 	let { field } = props
 	let { cellOnClick } = field
 
