fc282d52f2cd285694ebd7036b404627380774dd
yinlainghui <yinlianghui@hotoa.com>
2019-09-27 11:43:26 +0800

    应用程序启动器模块实现静态界面

--- src/components/dashboard/slds_dashboard.tsx
+++ src/components/dashboard/slds_dashboard.tsx
@@ -5,7 +5,8 @@ import PropTypes from 'prop-types';
 import { createAction } from '../../actions/views/grid';
 import styled from 'styled-components'
 import _ from 'underscore'
-import { Card, CardEmpty, CardFilter, Icon, DataTable, DataTableColumn, IconSettings } from '@salesforce/design-system-react';
+import { Card, CardEmpty, CardFilter, Icon, DataTable, DataTableColumn, IconSettings, 
+    AppLauncherTile, AppLauncherExpandableSection } from '@salesforce/design-system-react';
 
 const sampleItems = [
     { id: '1', name: 'Cloudhub' },
@@ -33,6 +34,15 @@ let Cell = styled.div`
     padding: 12px;
     &.flex-split{
         flex: 0 0 50%;
+    }
+`;
+
+let AppLauncherDesktopInternal = styled.div`
+    padding: 0px 1rem;
+    .slds-section.slds-is-open{
+        .slds-section__content{
+            padding-top: 0px;
+        }
     }
 `;
 
@@ -128,13 +138,45 @@ class Dashboard extends React.Component {
                                     heading="应用程序启动器"
                                     icon={<Icon category="standard" name="document" size="small" />}
                                 >
-                                    <DataTable items={this.state.items} id="DataTableExample-1">
-                                        <DataTableColumn
-                                            label="Opportunity Name"
-                                            property="name"
-                                            truncate
-                                        />
-                                    </DataTable>
+                                    <AppLauncherDesktopInternal className="slds-app-launcher__content">
+                                        <AppLauncherExpandableSection title="Tile Section">
+                                            <AppLauncherTile
+                                                description="The primary internal Salesforce org. Used to run our online sales business and manage accounts."
+                                                iconText="SC"
+                                                title="Sales Cloud"
+                                            />
+                                            <AppLauncherTile
+                                                description="Salesforce Marketing Cloud lets businesses of any size engage with their customers through multiple channels of messaging."
+                                                iconBackgroundColor="#e0cf76"
+                                                iconText="MC"
+                                                title="Marketing Cloud"
+                                            />
+                                            <AppLauncherTile
+                                                description="Community for managing employee benefits and time off."
+                                                iconBackgroundColor="#6a8adc"
+                                                iconText="HR"
+                                                title="HR Concierge"
+                                            />
+                                            <AppLauncherTile
+                                                description="Manage your finances across multiple financial platforms and make the most of your capital."
+                                                iconBackgroundColor="#73c07b"
+                                                iconText="MM"
+                                                title="My Money"
+                                            />
+                                            <AppLauncherTile
+                                                description="The key to call center and contact center management is more simple than you think with this amazing application!"
+                                                iconBackgroundColor="#b67e6a"
+                                                iconText="CC"
+                                                title="Call Center"
+                                            />
+                                            <AppLauncherTile
+                                                description="Areas of Focus are used to track customer support for your brand and ensure high quality support"
+                                                iconBackgroundColor="#69bad0"
+                                                iconText="CS"
+                                                title="Customer Support Community"
+                                            />
+                                        </AppLauncherExpandableSection>
+                                    </AppLauncherDesktopInternal>
                                 </Card>
                             </div>
                         </IconSettings>
