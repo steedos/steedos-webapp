88c37d4b345bc008b61d85eaedd259cc200671fa
hotlong <zhuangjianguo@hotoa.com>
2019-10-18 16:46:43 +0800

    bootstrap 内置 iconPath，不再需要ICON_SETTINGS

--- src/stories/1-Dashboard.stories.js
+++ src/stories/1-Dashboard.stories.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import Dashboard from '../components/dashboard'
 import Bootstrap from '../components/bootstrap'
-import { IconSettings } from '@salesforce/design-system-react';
 import { Provider  } from 'react-redux';
 import store from '../stores/configureStore'
 
@@ -13,13 +12,11 @@ export default {
 export const instances = () => (
 
   <div className="App">
-    <IconSettings iconPath="/assets/icons" >
       <Provider store={store}>
         <Bootstrap>
           <Dashboard onClick={action('clicked')}/>
         </Bootstrap>
       </Provider>
-    </IconSettings>
   </div>
 
 )
