13b36a87f4f27197096a2addcb9191ca583d280f
yinlainghui <yinlianghui@hotoa.com>
2019-10-18 16:52:48 +0800

    storybook dashboard sections

--- src/stories/1-Dashboard.stories.js
+++ src/stories/1-Dashboard.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components'
 import { action } from '@storybook/addon-actions';
 import Dashboard from '../components/dashboard'
 import Bootstrap from '../components/bootstrap'
@@ -10,16 +11,42 @@ export default {
   title: 'Dashboard'
 };
 
-export const instances = () => (
+let CenterDiv = styled.div`
+  text-align: center;
+  height: 230px;
+  background: #fff;
+  border: solid 1px #eee;
+`;
 
+export const sections = () => (
   <div className="App">
     <IconSettings iconPath="/assets/icons" >
       <Provider store={store}>
         <Bootstrap>
-          <Dashboard onClick={action('clicked')}/>
+          <Dashboard
+            centerTopSection={(
+              <CenterDiv>
+                Center Top Section
+              </CenterDiv>
+            )}
+            centerBottomLeftSection={(
+              <CenterDiv>
+                Center Bottom Left Section
+              </CenterDiv>
+            )}
+            centerBottomRightSection={(
+              <CenterDiv>
+                Center Bottom Right Section
+              </CenterDiv>
+            )}
+            rightSection={(
+              <CenterDiv>
+                Right Section
+              </CenterDiv>
+            )}
+          />
         </Bootstrap>
       </Provider>
     </IconSettings>
   </div>
-
 )
