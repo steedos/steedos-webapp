7a710223a71e5692c31228b9884544386a0ee663
hotlong <zhuangjianguo@hotoa.com>
2019-10-18 13:49:34 +0800

    add storybook

---
+++ src/stories/1-Dashboard.stories.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Dashboard from '../components/dashboard'
+import Bootstrap from '../components/bootstrap'
+import { IconSettings } from '@salesforce/design-system-react';
+import { Provider  } from 'react-redux';
+import store from '../stores/configureStore'
+
+export default {
+  title: 'Dashboard',
+};
+
+export const instances = () => (
+
+    <div className="App">
+    <IconSettings iconPath="/assets/icons" >
+      <Provider store={store}>
+        <Bootstrap>
+          <Dashboard onClick={action('clicked')}/>
+        </Bootstrap>
+      </Provider>
+    </IconSettings>
+  </div>
+
+)
