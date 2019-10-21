190c4b7d9fd025820794ff54cdaaf25901aacec0
yinlainghui <yinlianghui@hotoa.com>
2019-10-21 09:37:35 +0800

    dashboard /story/widgets--widget-apps /story/widgets--widget-object

---
+++ src/stories/3-Widget.stories.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Bootstrap from '../components/bootstrap';
+import { Provider  } from 'react-redux';
+import store from '../stores/configureStore';
+import WidgetApps from '../components/widget_apps';
+import WidgetObject from '../components/widget_object';
+
+export default {
+  title: 'Widgets',
+};
+
+export const widgetApps = () => (
+  <Provider store={store}>
+    <Bootstrap>
+      <WidgetApps />
+    </Bootstrap>
+  </Provider>
+);
+
+export const widgetObject = () => (
+  <Provider store={store}>
+    <Bootstrap>
+      <WidgetObject label="待办任务" object_name="tasks" filters={[
+        ['assignees', '=', '{userId}'],
+        ['state', '<>', 'complete'],
+        ['due_date', 'between', 'this_year']
+      ]} columns={[{
+        label: "名称",
+        field: 'name',
+        href: true
+      }, {
+        label: "优先级",
+        field: 'priority'
+      }]} />
+    </Bootstrap>
+  </Provider>
+)
+
