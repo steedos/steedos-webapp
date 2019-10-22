bc36302e3acf03c41c4304d1bce8eed25df7f17d
hotlong <zhuangjianguo@hotoa.com>
2019-10-22 17:48:22 +0800

    export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,14 @@
 export function makeNewID(props){
     return props.id ? props.id : btoa(props.toString()) + Math.floor(Math.random() * 10000);
 }
+
+export {default as Bootstrap} from './bootstrap';
+export {default as Dashboard} from './dashboard';
+export {default as Grid} from './grid';
+export {default as Lookup} from './lookup';
+export {default as Organizations} from './organizations';
+export {default as SelectUsers} from './select_users';
+export {default as Tree} from './tree';
+export {default as WidgetApps} from './widget_apps';
+export {default as WidgetObject} from './widget_object';
+
