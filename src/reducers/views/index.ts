da523b05513d98320b0e9c8b6bb9971e310ca189
hotlong <zhuangjianguo@hotoa.com>
2019-10-23 09:11:02 +0800

    Create index.ts

---
+++ src/reducers/views/index.ts
@@ -0,0 +1,8 @@
+export {default as bootstrapReducer} from './bootstrap';
+export {default as dashboardReducer} from './dashboard';
+export {default as gridReducer} from './grid';
+export {default as lookupReducer} from './lookup';
+export {default as organizationsReducer} from './organizations';
+export {default as selectUsersReducer} from './select_users';
+export {default as treeReducer} from './tree';
+export {default as widgetInstanceReducer} from './widget_instance';
