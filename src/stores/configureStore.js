468a4a6fdf3ca1eff2629e617d56e7a819709582
baozhoutao <baozhoutao@hotoa.com>
2019-10-23 10:04:43 +0800

    fix bug

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import rootReducer from '../reducers'
+import { rootReducer } from '../reducers'
 import { composeWithDevTools } from 'redux-devtools-extension';
 const composeEnhancers = composeWithDevTools({ realtime: true});
 
