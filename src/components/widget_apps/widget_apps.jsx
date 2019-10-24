95982b3cc04bf7aeb68033ce00c817881a86caa5
yinlainghui <yinlianghui@hotoa.com>
2019-10-24 09:48:10 +0800

    fix app.url也支持RootURL

--- src/components/widget_apps/widget_apps.jsx
+++ src/components/widget_apps/widget_apps.jsx
@@ -50,7 +50,7 @@ class WidgetApps extends React.Component {
                     if(app.url){
                         url = app.url;
                     }
-                    if (!/^http:\/\//.test(this.url)) {
+                    if (!/^http:\/\//.test(url)) {
                         if (window.__meteor_runtime_config__)
                             url = window.__meteor_runtime_config__.ROOT_URL_PATH_PREFIX + url;
                     }
