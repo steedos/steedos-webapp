8360d290d805ff0c5256c00a419042ee42355c32
殷亮辉 <yinlianghui@hotoa.com>
2019-12-04 15:35:26 +0800

    通知中心：头像显示异常
    https://github.com/steedos/creator/issues/1287

--- src/actions/views/notifications.ts
+++ src/actions/views/notifications.ts
@@ -18,6 +18,7 @@ export function loadNotificationsData(options: any) {
             { field: "url" },
             { field: "owner" },
             { field: "is_read" },
+            { field: "from" },
             { field: "created" }
         ]
     });
