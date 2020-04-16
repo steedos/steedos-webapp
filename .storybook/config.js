9994b38be52cdb5bf39d38c2b6fba64c1faf6e6b
殷亮辉 <yinlianghui@hotoa.com>
2020-04-16 11:54:35 +0800

    增加pager组件
    https://github.com/steedos/creator/issues/1656

--- .storybook/config.js
+++ .storybook/config.js
@@ -9,6 +9,10 @@ addParameters({
     code: `<link rel="stylesheet" type="text/css" href="` + REACT_APP_API_BASE_URL + `/assets/styles/salesforce-lightning-design-system.min.css"></link>`,
     picked: true,
   }, {
+    id: `bootstrap`,
+    code: `<link rel="stylesheet" type="text/css" href="/assets/styles/bootstrap.css"></link>`,
+    picked: true,
+  }, {
     id: `style`,
     code: `<style>html{background: #fff;}</style>`,
     picked: true,
