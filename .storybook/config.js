f1f25fd78c16821024b90f633256a5899f99981c
殷亮辉 <yinlianghui@hotoa.com>
2020-06-08 15:50:19 +0800

    fix 样式间隔和文字大小出问题了

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,12 +5,12 @@ const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 addDecorator(withCssResources)
 addParameters({
   cssresources: [{
-    id: `sfds`,
-    code: `<link rel="stylesheet" type="text/css" href="` + REACT_APP_API_BASE_URL + `/assets/styles/salesforce-lightning-design-system.min.css"></link>`,
-    picked: true,
-  }, {
     id: `bootstrap`,
     code: `<link rel="stylesheet" type="text/css" href="/assets/styles/bootstrap.css"></link>`,
+    picked: true,
+  }, {
+    id: `sfds`,
+    code: `<link rel="stylesheet" type="text/css" href="` + REACT_APP_API_BASE_URL + `/assets/styles/salesforce-lightning-design-system.min.css"></link>`,
     picked: true,
   }, {
     id: `style`,
